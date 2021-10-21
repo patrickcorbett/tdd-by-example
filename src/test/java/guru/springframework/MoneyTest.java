7be4ef590b8650cd1ea2f5f33c1138054a6fd9dc
patrick.corbett@infomotion.de <patrick.corbett@infomotion.de>
2021-10-21 17:59:02 +0200

    #18 - Degenerate / Immutable Objects "Side Effects"

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -11,8 +11,10 @@ class MoneyTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assertions.assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        Assertions.assertEquals(10, product.amount);
+        product = five.times(3);
+        Assertions.assertEquals(15, product.amount);
     }
 
 }
