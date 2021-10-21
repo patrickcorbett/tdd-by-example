f00b44571f35783c04a660416a17fdad10233128
patrick.corbett@infomotion.de <patrick.corbett@infomotion.de>
2021-10-21 18:27:28 +0200

    #21 - New Currency "Franc" - Basic Test Driven Development apporach, Write test first, make it compile and then flesh it out

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class MoneyTest {
 
     @Test
-    void testMultiplication() {
+    void testMultiplicationDollar() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         Assertions.assertEquals(new Dollar(10), product);
@@ -18,9 +18,24 @@ class MoneyTest {
     }
 
     @Test
-    void testEquality() {
+    void testEqualityDollar() {
         Assertions.assertEquals(new Dollar(5), new Dollar(5));
         Assertions.assertNotEquals(new Dollar(5), new Dollar(8));
+    }
+
+    @Test
+    void testMultiplicationFranc() {
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        Assertions.assertEquals(new Franc(10), product);
+        product = five.times(3);
+        Assertions.assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    void testEqualityFranc() {
+        Assertions.assertEquals(new Franc(5), new Franc(5));
+        Assertions.assertNotEquals(new Franc(5), new Franc(8));
     }
 
 }
