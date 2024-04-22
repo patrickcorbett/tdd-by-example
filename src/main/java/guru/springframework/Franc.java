83d186c77a258c64e0c40ddef929a10db85371c4
Patrick Corbett <patrick.corbett@live.com>
2024-04-22 18:47:30 +0200

    #22 - TDD Chapter 6 - Equality for all, Redux - Single Equals for Dollar and Franc via a Money parent class

--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -3,9 +3,7 @@ package guru.springframework;
 /**
  * @author Patrick Corbett
  */
-public class Franc {
-
-    private int amount;
+public class Franc extends Money {
 
     public Franc(int pAmount) {
         amount = pAmount;
@@ -14,11 +12,4 @@ public class Franc {
     Franc times(int pMultiplier) {
         return new Franc(amount * pMultiplier);
     }
-
-    public boolean equals(Object object) {
-        // Manual equals, left like this as equals with null or other class will be done later
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
-    }
-
 }
